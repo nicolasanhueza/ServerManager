f4c16ee4a5abc4ac8847107ea173db1f6641ef86
nicolasanhueza <nicolasanhuezajaque@hotmail.com>
2024-03-20 12:16:20 -0300

    edit file server_manager.py, adding automatic directory grab functionality

--- server_manager.py
+++ server_manager.py
@@ -2,26 +2,37 @@ import subprocess
 from datetime import datetime
 import os
 
-def git_init():
-    # Cambiar al directorio deseado
-    os.chdir(r'C:\Users\nicol\AppData\Roaming\.minecraft\essential_mod\forge\1.20.1\1.20.1 Forge Essential\saves\New World')
-    # Inicializar el repositorio Git
+def get_save_directory():
+    home_directory = os.path.expanduser("~")
+    # World route
+    return os.path.join(home_directory, "AppData", "Roaming", ".minecraft", "essential_mod", "forge", "1.20.1", "1.20.1 Forge Essential", "saves", "New World")
+
+def git_init(directory):
+    # Directory to start git
+    os.chdir(directory)
+    # Initialize the Git repository
     subprocess.run(['git', 'init'])
 
 def git_pull():
+    # To update existing changes in the repository that are not local
     subprocess.run(['git', 'pull'])
 
 def git_add_commit_push():
+    # To upload changes to the repository
     subprocess.run(['git', 'add', '.'])
+    # Get date and time
     now = datetime.now()
     formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+    # We customize the commit message plus the date and time
     commit_message = f"Watakacraft [{formatted_date}]"
     subprocess.run(['git', 'commit', '-m', commit_message])
+    # Improvable?
     subprocess.run(['git', 'push', '--set-upstream', 'https://github.com/nicolasanhueza/MundoPrueba.git', 'master'])
+    
 def main():
     git_init()
-    option = input("¿Desea actualizar (U) o guardar (G)? ").upper()
-    if option == 'U':
+    option = input("¿Desea Actualizar (A) o Guardar (G)? ").upper()
+    if option == 'A':
         git_pull()
     elif option == 'G':
         git_add_commit_push()
